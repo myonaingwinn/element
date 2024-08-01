266830bcf78529209ad9455ac2ac36985d16e8ee
Myo Naing Win <myo-naing-win@mki.co.jp>
2024-08-01 18:09:26 +0900

    i18n: add Burmese translation

---
+++ src/locale/lang/my.js
@@ -0,0 +1,123 @@
+export default {
+  el: {
+    colorpicker: {
+      confirm: 'အိုကေ',
+      clear: 'ရှင်းမယ်',
+    },
+    datepicker: {
+      now: 'ယခု',
+      today: 'ယနေ့',
+      cancel: 'ပယ်ဖျက်',
+      clear: 'ရှင်းမယ်',
+      confirm: 'အိုကေ',
+      selectDate: 'ရက်စွဲကိုရွေးပါ',
+      selectTime: 'အချိန်ကိုရွေးပါ',
+      startDate: 'စတင်မည့်ရက်စွဲ',
+      startTime: 'စတင်မည့်အချိန်',
+      endDate: 'ကုန်ဆုံးမည့်ရက်စွဲ',
+      endTime: 'ကုန်ဆုံးမည်းအချိန်',
+      prevYear: 'အရင်နှစ်',
+      nextYear: 'နောက်နှစ်',
+      prevMonth: 'အရင်လ',
+      nextMonth: 'နောက်လ',
+      year: '',
+      month1: 'ဇန်နဝါရီ',
+      month2: 'ဖေဖော်ဝါရီ',
+      month3: 'မတ်',
+      month4: 'ဧပြီ',
+      month5: 'မေ',
+      month6: 'ဇွန်',
+      month7: 'ဇူလိုင်',
+      month8: 'သြဂုတ်',
+      month9: 'စက်တင်ဘာ',
+      month10: 'အောက်တိုဘာ',
+      month11: 'နိုဝင်ဘာ',
+      month12: 'ဒီဇင်ဘာ',
+      week: 'ရက်သတ္တပတ်',
+      weeks: {
+        sun: 'နွေ',
+        mon: 'လာ',
+        tue: 'ဂါ',
+        wed: 'ဟူး',
+        thu: 'တေး',
+        fri: 'သော',
+        sat: 'နေ',
+      },
+      months: {
+        jan: 'ဇန်',
+        feb: 'ဖေ',
+        mar: 'မတ်',
+        apr: 'ပြီ',
+        may: 'မေ',
+        jun: 'ဇွန်',
+        jul: 'ဇူ',
+        aug: 'ဩ',
+        sep: 'စက်',
+        oct: 'အောက်',
+        nov: 'နို',
+        dec: 'ဒီ',
+      },
+    },
+    select: {
+      loading: 'ခဏစောင့်ပေးပါ',
+      noMatch: 'ကိုက်ညီသောဒေတာမရှိပါ',
+      noData: 'ဒေတာမရှိပါ',
+      placeholder: 'ရွေးပါ',
+    },
+    cascader: {
+      noMatch: 'ကိုက်ညီသောဒေတာမရှိပါ',
+      loading: 'ခဏစောင့်ပေးပါ',
+      placeholder: 'ရွေးပါ',
+      noData: 'ဒေတာမရှိပါ',
+    },
+    pagination: {
+      goto: 'ဆီသို့',
+      pagesize: '/စာမျက်နှာ',
+      total: 'စုစုပေါင်း {total}',
+      pageClassifier: '',
+    },
+    messagebox: {
+      title: 'မက်ဆေ့ချ်',
+      confirm: 'အိုကေ',
+      cancel: 'ပယ်ဖျက်',
+      error: 'မမှန်ကန်သော ထည့်သွင်းမှု',
+    },
+    upload: {
+      deleteTip: 'ဖျက်ရန် ကိုနှိပ်ပြီးဖယ်ရှားပါ',
+      delete: 'ဖျက်',
+      preview: 'အစမ်းကြည့်',
+      continue: 'ဆက်လက်',
+    },
+    table: {
+      emptyText: 'ဒေတာမရှိ',
+      confirmFilter: 'အိုကေ',
+      resetFilter: 'ပြန်လည်သတ်မှတ်',
+      clearFilter: 'အားလုံး',
+      sumText: 'ပေါင်းလဒ်',
+    },
+    tree: {
+      emptyText: 'ဒေတာမရှိ',
+    },
+    transfer: {
+      noMatch: 'ကိုက်ညီသောဒေတာမရှိပါ',
+      noData: 'ဒေတာမရှိ',
+      titles: ['စာရင်း ၁', 'စာရင်း ၂'], // to be translated
+      filterPlaceholder: 'သော့ချက်စာလုံးကို ရိုက်ထည့်ပါ။', // to be translated
+      noCheckedFormat: '{total} ခု', // to be translated
+      hasCheckedFormat: '{checked}/{total} စိစစ်ပြီး', // to be translated
+    },
+    image: {
+      error: 'မအောင်မြင်ပါ',
+    },
+    pageHeader: {
+      title: 'ပြန်မည်', // to be translated
+    },
+    popconfirm: {
+      confirmButtonText: 'အိုကေ',
+      cancelButtonText: 'ပယ်ဖျက်',
+    },
+    empty: {
+      description: 'ဒေတာမရှိ',
+    },
+  },
+}
